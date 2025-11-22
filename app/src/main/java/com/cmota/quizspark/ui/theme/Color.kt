6d1540d805093b98fa39377a26e48871e5b12b1b
Carlos Mota <478962+cmota@users.noreply.github.com>
2025-11-22 11:27:01 +0100

    Added Codelab

--- app/src/main/java/com/cmota/quizspark/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.cmota.quizspark.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val darkGreenBackground = Color(0xFF111F16)
-val brightGreen = Color(0xFF00E955)
-val mediumGreen = Color(0xFF0c4120)
-val hintGray = Color(0xFFA0A0A0)
-val borderGray = Color(0x809E9E9E)
